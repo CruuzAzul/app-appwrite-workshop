50c060c436ed275ce97b55f278d9cfe001891566
CruuzAzul <alves.mckl@gmail.com>
2023-07-23 19:42:47 +0200

    🔓 Create register page + découverte du web

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,13 +1,27 @@
+import {ID, Models} from 'appwrite';
+
 import {Destination} from '@/models/destination';
 
-import {database} from './appwrite';
+import {account, database} from './appwrite';
 import {Server} from './server';
 
 export const getDestinations = async (): Promise<Destination[]> => {
-	const {documents: destinations} = await database.listDocuments<Destination>(
+  const {documents: destinations} = await database.listDocuments<Destination>(
 		Server.databaseId,
 		Server.destinationCollectionId
 	);
 
 	return destinations;
+};
+
+export const createAccount = async (
+	email: string,
+	password: string,
+	name: string
+): Promise<Models.User<Models.Preferences>> => {
+  const session = await account.create(ID.unique(), email, password, name);
+
+  await account.createEmailSession(email, password);
+
+  return session;
 };
