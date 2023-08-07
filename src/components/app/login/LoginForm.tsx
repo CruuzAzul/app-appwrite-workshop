2dd408ccdf0ac7922778357864deec72e79e3eed
Alves Mickaël <alves.mckl@gmail.com>
2023-08-07 22:30:38 +0200

    🔓 Create login page (#11)

---
+++ src/components/app/login/LoginForm.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import Link from 'next/link';
+
+import {PasswordInput} from '@/components/common/PasswordInput';
+import {TextInput} from '@/components/common/TextInput';
+
+export const LoginForm: React.FC<{
+	onSubmit: (e: React.FormEvent<EventTarget>) => Promise<void>;
+	setEmail: (value: ((prevState: string) => string) | string) => void;
+	setPassword: (value: ((prevState: string) => string) | string) => void;
+	email: string;
+	password: string;
+}> = ({onSubmit, setEmail, setPassword, email, password}) => {
+	return (
+		<section className="u-flex u-main-center u-cross-center u-full-screen-height u-flex-vertical u-gap-32">
+			<h1 className="eyebrow-heading-1 u-text-center u-color-text-pink">
+				Accédez à votre compte
+			</h1>
+			<div className="card u-width-600 u-min-width-200">
+				<form
+					onSubmit={onSubmit}
+					className="u-flex u-flex-vertical u-row-gap-24"
+				>
+					<ul className="form-list">
+						<TextInput label="Email" setValue={setEmail} type="email" />
+						<PasswordInput setValue={setPassword} />
+					</ul>
+					<div className="u-flex u-flex-vertical u-main-center u-cross-center">
+						<button
+							type="submit"
+							disabled={!email || !password}
+							className="button"
+						>
+							Connexion
+						</button>
+						<p className="u-padding-block-start-16">
+							Vous n&apos;avez pas encore un compte ?{' '}
+							<Link href="/register" className="link u-color-text-info	">
+								Inscrivez-vous
+							</Link>
+						</p>
+					</div>
+				</form>
+			</div>
+		</section>
+	);
+};
