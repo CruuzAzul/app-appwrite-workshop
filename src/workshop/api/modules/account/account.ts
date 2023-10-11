0d634124c2b8ed84559fa3ffa47365eded45f40c
CruuzAzul <alves.mckl@gmail.com>
2023-10-12 00:52:01 +0200

    🫳🏼 Setup workshop code

--- src/workshop/api/modules/account/account.ts
+++ src/workshop/api/modules/account/account.ts
@@ -1,14 +1,18 @@
-import {AppwriteException, ID} from 'appwrite';
-
 import {UserType} from '@/types/UserHook.type';
-import {account} from '@/workshop/api/config/client.config';
 
 export async function login(email: string, password: string): Promise<void> {
 	try {
-		await account.createEmailSession(email, password);
+		/**
+		 * ----------------------------------------
+		 * HERE : Session creation code (login)
+		 * ----------------------------------------
+		 */
 	} catch (error) {
-		const appwriteException = error as AppwriteException;
-		console.error(appwriteException.message);
+		/**
+		 * ----------------------------------------
+		 * HERE : Appwrite error handling here
+		 * ----------------------------------------
+		 */
 	}
 }
 
@@ -19,18 +23,27 @@ export async function register(
 	login: (email: string, password: string) => Promise<void>
 ): Promise<UserType | undefined> {
 	try {
-		const session = await account.create(ID.unique(), email, password, name);
-		await login(email, password);
-
-		return session;
+		/**
+		 * ----------------------------------------
+		 * HERE : Account creation code (register)
+		 * It looks like we need to return a `session` here
+		 * ----------------------------------------
+		 */
 	} catch (error) {
-		const appwriteException = error as AppwriteException;
-		console.error(appwriteException.message);
+		/**
+		 * ----------------------------------------
+		 * HERE : Appwrite error handling here
+		 * ----------------------------------------
+		 */
 	}
 }
 
 export async function logout() {
-	await account.deleteSession('current');
+	/**
+	 * ----------------------------------------
+	 * HERE : Session logout code
+	 * ----------------------------------------
+	 */
 }
 
 export async function socialLogin(
@@ -39,13 +52,16 @@ export async function socialLogin(
 	failureRedirectUrl: string
 ) {
 	try {
-		account.createOAuth2Session(
-			provider,
-			successRedirectUrl,
-			failureRedirectUrl
-		);
+		/**
+		 * ----------------------------------------
+		 * HERE : Session connection code (social login)
+		 * ----------------------------------------
+		 */
 	} catch (error: any) {
-		const appwriteException = error as AppwriteException;
-		console.error(appwriteException.message);
+		/**
+		 * ----------------------------------------
+		 * HERE : Appwrite error handling here
+		 * ----------------------------------------
+		 */
 	}
 }
--- src/workshop/components/database/CoordinatesCardsList.tsx
+++ src/workshop/components/database/CoordinatesCardsList.tsx
@@ -2,13 +2,8 @@
 
 import {FC, useEffect, useState} from 'react';
 
-import {RealtimeResponseEvent} from 'appwrite';
-
 import {CoordinatesCard} from '@/components/app/database/coordinates/CoordinatesCard';
 import {Coordinates} from '@/models/coordinates';
-import {EventType, getEventType} from '@/utils/realtime.utils';
-import {AppwriteClient} from '@/workshop/api/config/client.config';
-import {EnvConfig} from '@/workshop/api/config/env.config';
 
 interface CoordinatesCardsListProps {
 	coordinatesList: Coordinates[];
@@ -21,39 +16,14 @@ export const CoordinatesCardsList: FC<CoordinatesCardsListProps> = ({
 		useState(coordinatesList);
 
 	useEffect(() => {
-		const coordinatesCollection = `databases.${EnvConfig.databaseId}∆.collections.${EnvConfig.coordinatesCollectionId}.documents`;
-
-		const unsubscribe = AppwriteClient.subscribe(
-			coordinatesCollection,
-			(response: RealtimeResponseEvent<Coordinates>) => {
-				const eventType = getEventType({
-					events: response.events,
-				});
-
-				switch (eventType) {
-					case EventType.CREATE:
-						setUpdatedCoordinatesList((currentCoordinatesList) => [
-							response.payload as Coordinates,
-							...currentCoordinatesList,
-						]);
-						break;
-					case EventType.DELETE:
-						const deletedItemId = response.payload.$id;
-						setUpdatedCoordinatesList((currentCoordinatesList) =>
-							currentCoordinatesList.filter(
-								(item) => item.$id !== deletedItemId
-							)
-						);
-						break;
-					default:
-						break;
-				}
-			}
-		);
-
-		return () => {
-			unsubscribe();
-		};
+		/**
+		 * ----------------------------------------
+		 * HERE : Subscribe to the realtime events on the coordinates collection
+		 * and update the coordinates list accordingly
+		 *
+		 * Hint: You can use the getEventType function to know if the event is a creation, update or deletion.
+		 * ----------------------------------------
+		 */
 	}, []);
 
 	return (
