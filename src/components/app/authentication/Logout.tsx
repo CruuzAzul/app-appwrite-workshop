99ea1ecb47d88eb8178a2121368aac0225d967c5
CruuzAzul <alves.mckl@gmail.com>
2023-10-16 23:48:22 +0200

    🗑️ Remove appwrite badge

--- src/components/app/authentication/Logout.tsx
+++ src/components/app/authentication/Logout.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import Image from 'next/image';
-
 import {UseAccount} from '@/hooks/useAccount';
 
 export const Logout = () => {
@@ -12,14 +10,5 @@ export const Logout = () => {
 			<span className="text">Logout</span>
 			<span className="icon-logout-right" aria-hidden="true" />
 		</button>
-	) : (
-		<a href="https://appwrite.io/" target="_blank" className="u-width-200">
-			<Image
-				width={200}
-				height={54}
-				src="https://appwrite.io/images-ee/press/badge-pink-button.svg"
-				alt="Built with Appwrite"
-			/>
-		</a>
-	);
+	) : null;
 };
